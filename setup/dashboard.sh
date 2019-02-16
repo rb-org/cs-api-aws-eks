8977f5402fa5cf92acd00d57cfd2f9c3b8059f82
Rupert Broad <rupert.broad@gmail.com>
2019-02-16 19:28:55 +0100

    update output

--- setup/dashboard.sh
+++ setup/dashboard.sh
@@ -16,3 +16,4 @@ else
     echo "Dashboard URL:  'http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/'"
 fi
 
+# http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/login
