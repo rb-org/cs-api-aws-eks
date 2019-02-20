1562f390ae80a58315417917ba3fbfebd993aacd
Rupert Broad <rupert.broad@gmail.com>
2019-02-20 10:54:26 +0100

    .

--- .circleci/install_awscli.sh
+++ .circleci/install_awscli.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # Install AWS CLI
-apk add python3
-apk add --update py-pip
+apk add python3 py-pip
 pip3 install --upgrade pip
-pip3 install awscli --upgrade --user
+pip3 install awscli --upgrade
 export PATH=~/.local/bin:$PATH
 echo 'export PATH=/.local/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
