2cd76df1947f6414f12fcee7648a0d870d3fbe7b
Rupert Broad <rupert.broad@gmail.com>
2019-02-20 10:58:07 +0100

    .

--- .circleci/install_aws_iam_auth.sh
+++ .circleci/install_aws_iam_auth.sh
@@ -2,6 +2,9 @@
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 cp ./aws-iam-authenticator /usr/local/aws-iam-authenticator 
+cp ./aws-iam-authenticator /bin/aws-iam-authenticator 
 export PATH=/usr/local:$PATH
 echo 'export PATH=/usr/local:$PATH' >> ~/.bashrc
+export PATH=/bin:$PATH
+echo 'export PATH=/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
