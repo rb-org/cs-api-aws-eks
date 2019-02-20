91b9780c5c4eed3c1f9a926d6fc511cebcd447f8
Rupert Broad <rupert.broad@gmail.com>
2019-02-20 11:04:43 +0100

    remove setup

--- setup/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  account_id       = "${data.aws_caller_identity.current.account_id}"
-  region           = "${data.aws_region.current.name}"
-  enable_dashboard = "${var.enable_dashboard ? 1:0}"
-}
