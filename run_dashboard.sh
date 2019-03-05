e58859f12349a30f700dba6ac256faafa9b8cac8
Rupert Broad <rupert.broad@gmail.com>
2019-03-05 23:26:51 +0100

    update readme

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -16,4 +16,3 @@ else
     echo "Dashboard URL:  'http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/'"
 fi
 
-# http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/login
